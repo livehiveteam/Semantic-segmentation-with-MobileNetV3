8ca221b928ae5f57dea459f37bfc68ff01e30ee8
seunghun.cho <seunghun.cho@code42.ai>
2020-04-07 15:39:42 +0900

    test 448 small model

--- test.py
+++ test.py
@@ -21,15 +21,17 @@ test_imgs = os.listdir(TEST_DIR)
 
 device = 'GPU:0'
 model_name = 'mobilenet_small' # 'mobilenet_small'
+input_size = 448
 
 #old_model_path = '/workdir/data/experiments/fb_combined_mobilenetv3_tf_coco_pixart_supervisely/model_best_0.12529000639915466.h5'
 #old_model_path = '/workdir/data/experiments/picsart_supervisely/model_best_0.3700900077819824.h5' # large
-old_model_path = '/workdir/data/experiments/picsart_supervisely/model_best_0.26719000935554504.h5' # small
+#old_model_path = '/workdir/data/experiments/picsart_supervisely/model_best_0.26719000935554504.h5' # small
+old_model_path = '/workdir/data/experiments/picsart/model_best_0.16599999368190765.h5' # small
 
 mobilenet_model = Model(device=device,
                         model_name=model_name,
                         n_class=1,
-                        input_shape=(1,224,224,3),
+                        input_shape=(1,input_size,input_size,3),
                         old_model_path=old_model_path)
                         
 for i, img_path in enumerate(test_imgs):
@@ -41,18 +43,20 @@ for i, img_path in enumerate(test_imgs):
 
     test_img = cv2.imread(img_path)
     test_img = test_img[:,:,::-1]
-    test_img = cv2.resize(test_img, (224,224))
+    test_img = cv2.resize(test_img, (input_size,input_size))
     test_tensor = ToTensorColor()(test_img)
     test_tensor = tf.expand_dims(test_tensor, 0)
     out = mobilenet_model.predict(test_tensor)
-    #print(out.shape)
     out_img = np.squeeze(out)
-    #print(out_img.shape)
+    print(out_img.shape)
+    print("--")
+
+    test_img = cv2.resize(test_img, (224,224))
     plt.imshow(test_img)
     plt.imshow((out_img>0.5)*255, alpha=0.4)
     plt.show()
 
     plt.savefig(RESULT_DIR + img_name + '.png')
 
-    if i > 9:
+    if i > 29:
         break
